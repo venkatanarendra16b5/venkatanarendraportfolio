288396d2a1de00f972ed6253138eb983d8edce66
Venkata Narendra <97586837+venkatanarendra16b5@users.noreply.github.com>
2025-10-26 04:58:50 +0000

    loadfix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production'
-    ? '/venkatanarendraportfolio/'
-    : '/',
+  base: '/venkatanarendraportfolio/', // 👈 exact repo name
 })
